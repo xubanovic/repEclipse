11c898478d9795a0af2a52210e535e20cb063ce1
Xubanovic <xubanovic@gmail.com>
2024-10-17 11:32:06 +0200

    Froga proiektua

---
+++ FrogaProiektua/src/kaixoMundua/kaixoMundua.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package kaixoMundua;
+
+/**
+ * 
+ */
+public class kaixoMundua {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kaixo Mundua"); 
+	}
+
+}
