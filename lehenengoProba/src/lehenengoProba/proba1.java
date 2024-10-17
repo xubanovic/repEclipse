578ca895f3af0f3c9bb316ca88780d2c659597d6
Xubanovic <xubanovic@gmail.com>
2024-10-17 11:30:51 +0200

    lehenengo proba

---
+++ lehenengoProba/src/lehenengoProba/proba1.java
@@ -0,0 +1,11 @@
+package lehenengoProba;
+
+public class proba1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1 proba"); 
+
+		System.out.println("2 proba"); 
+	}
+
+}
