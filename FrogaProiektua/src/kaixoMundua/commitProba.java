5f6c00272142acd346c80d72177eced7ddec6a02
Xubanovic <xubanovic@gmail.com>
2024-10-17 10:58:47 +0200

    2. Commit-a

--- FrogaProiektua/src/kaixoMundua/commitProba.java
+++ FrogaProiektua/src/kaixoMundua/commitProba.java
@@ -3,6 +3,7 @@ package kaixoMundua;
 public class commitProba {
 	public static void main(String[] args) {
 		
-		System.out.println("Lehenengo Commit-a"); 
+		System.out.println("Lehenengo Commit-a");
+		System.out.println("2. Commit-a");
 	}
 }
